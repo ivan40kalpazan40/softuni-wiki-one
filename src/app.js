1795dd6f696cc3295fad2d3350a32477a1a6761c
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-04 21:47:11 +0300

    fix to views setup to render from subfolders fix links in main.hbs to correctly load static files

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
 const path = require('path');
-const handlebars = require('express-handlebars');
-
+const templateInit = require('../config/handlebars');
 const app = express();
 
+templateInit(app);
 // STATIC FOLDER SETUP
 app.use(express.static(path.resolve(__dirname, 'public')));
-// Handlebars VIEW ENGINE SETUP
-app.set('views', path.resolve(__dirname, 'views'));
-app.engine('hbs', handlebars({ extname: 'hbs' }));
-app.set('view engine', 'hbs');
 app.use(routes);
 
 app.listen(5000, console.log.bind(console, 'Server working on port 5000....'));
