03bf7deec086a9cb90b20d63dca3acfbc20105c7
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-04 19:07:45 +0300

    change template engine from ejs to handlebars

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const routes = require('./routes');
 const path = require('path');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
 // STATIC FOLDER SETUP
 app.use(express.static(path.resolve(__dirname, 'public')));
-// EJS VIEW ENGINE SETUP
-app.set('views', path.resolve('./src/views'));
-app.set('view engine', 'ejs');
-
+// Handlebars VIEW ENGINE SETUP
+app.set('views', path.resolve(__dirname, 'views'));
+app.engine('hbs', handlebars({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
 app.use(routes);
 
 app.listen(5000, console.log.bind(console, 'Server working on port 5000....'));
