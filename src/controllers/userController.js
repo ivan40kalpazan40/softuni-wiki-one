450d0107f76f48bac30c45c3a02a75effc398f26
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-07 20:01:57 +0300

    register user logic

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -14,33 +14,32 @@ const renderRegister = (req, res) => {
 };
 const registerUser = async (req, res) => {
   const { username, password, password2 } = req.body;
-  const result = await generalServices.cryptPassword(password);
-  console.log(`*** ${result}`);
   const isConfirmed = generalServices.confirmPassword(password, password2);
   if (isConfirmed) {
-    userServices
-      .getUser(username)
-      .then((user) => {
-        const isExisting = Boolean(user);
-        if (isExisting) {
-          throw new Error(`User ${user.username} already exists!`);
-        }
-        // REGISTER USER HERE
-        userServices
-          .register(username, password)
-          .then((user) => {
-            console.log(user);
-            res.redirect('/');
-          })
-          .catch((err) => {
-            console.log(err.message);
-            res.redirect('/user/register');
-          });
-      })
-      .catch((err) => {
-        console.log(`ERR::`, err.message);
-        res.redirect('/user/register');
-      });
+    userServices.getUser(username).then((user) => {
+      const isExisting = Boolean(user);
+      if (isExisting) {
+        throw new Error(`User ${user.username} already exists!`);
+      }
+      // REGISTER USER HERE
+      generalServices
+        .cryptPassword(password)
+        .then((hashed) => {
+          userServices
+            .register(username, hashed)
+            .then((user) => {
+              res.redirect('/');
+            })
+            .catch((err) => {
+              console.log(err.message);
+              res.redirect('/user/register');
+            });
+        })
+        .catch((err) => {
+          console.log(`ERR::`, err.message);
+          res.redirect('/user/register');
+        });
+    });
   } else {
     const error = new Error('Passwords shoud match!');
     console.log(`ERR::: ${error.message}`);
